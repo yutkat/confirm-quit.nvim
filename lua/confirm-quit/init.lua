8da03df423c8e1a779da5d26795b23b3f3609fe7
Alex Mozaidze <alexman2622@gmail.com>
2023-05-08 02:43:58 +0400

    refactor: improved logic handling and spacing

--- lua/confirm-quit/init.lua
+++ lua/confirm-quit/init.lua
@@ -4,35 +4,44 @@ local config = require("confirm-quit.config")
 local function is_last_window()
 	local wins = vim.api.nvim_list_wins()
 	local count = 0
+
 	for _, v in ipairs(wins) do
 		local win = vim.api.nvim_win_get_config(v).relative
+
 		if win == "" then
 			count = count + 1
 		end
 	end
-	if count == 1 then
-		return true
-	end
-	return false
+
+	return count == 1
 end
 
 function M.confirm_quit()
-	if is_last_window() and vim.fn.tabpagenr("$") == 1 then
-		if vim.fn.confirm("Do you want to quit?", "&Yes\n&No", 2) ~= 1 then
-			return
-		end
+	local is_last_tab_page = vim.fn.tabpagenr("$") == 1
+
+	if not is_last_window() then
+		vim.cmd.quit()
+		return
 	end
-	vim.cmd.quit()
+	if not is_last_tab_page then
+		vim.cmd.quit()
+		return
+	end
+
+	if vim.fn.confirm("Do you want to quit?", "&Yes\n&No", 2, "Question") == 1 then
+		vim.cmd.quit()
+	end
 end
 
 local function setup_cmdline_quit()
-	if config.options.overwrite_q_command == true then
-		vim.cmd([[cnoreabbrev <expr> q (getcmdtype() ==# ":" && getcmdline() ==# "q") ? "ConfirmQuit" : "q" ]])
-		vim.api.nvim_create_user_command("ConfirmQuit", function()
-			require("confirm-quit").confirm_quit()
-		end, { force = true })
-		vim.cmd([[cnoreabbrev qq  quit]])
+	if config.options.overwrite_q_command then
+		vim.cmd([[ cnoreabbrev <expr> q (getcmdtype() == ":" && getcmdline() ==# "q") ? "ConfirmQuit" : "q" ]])
+		vim.cmd([[ cnoreabbrev qq quit ]])
 	end
+
+	vim.api.nvim_create_user_command("ConfirmQuit", function()
+		require("confirm-quit").confirm_quit()
+	end, { force = true })
 end
 
 function M.setup(user_conf)
